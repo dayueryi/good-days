731f46375ddb6a6d256168e54abe0d3952e2c3c6
wjw <283503511@qq.com>
2018-03-29 18:05:35 +0800

    2

---
+++ web/src/components/Home/home.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './home.scss';
+class Home extends Component {
+    state = {}
+    render() { 
+        return 
+    }
+}
+ 
+export default Home;
---
+++ web/src/components/login/login.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './login.scss';
+class Login extends Component {
+    state = {}
+    render() { 
+        return 
+    }
+}
+ 
+export default Login;
